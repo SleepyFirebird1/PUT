0f4da68dd61f5fa15d253c1aebe19436b5ee48b2
Marcin Jakubowski <sleepyfirebird@MacBook-Pro-Marcin.local>
2025-10-17 10:48:19 +0200

    Results check and compare

--- Combinatorial_optimization/main.py
+++ Combinatorial_optimization/main.py
@@ -2,7 +2,7 @@ from graph.graph_converter import read_from_file, matrix_from_edges
 from algorithms.greedy_algorithm import greedy_coloring
 
 if __name__ == "__main__":
-    file_path = "./utils/instance.txt"
+    file_path = "./utils//instance/le450_5a.txt"
     num_nodes, edges = read_from_file(file_path)
     adjacency_matrix = matrix_from_edges(num_nodes, edges)
     print(greedy_coloring(adjacency_matrix))
