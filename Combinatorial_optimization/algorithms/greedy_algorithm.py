65973953d79f8f7ab5548f64045ea41fda04355e
gaucho212 <adamwoz2005@gmail.com>
2025-10-22 14:57:05 +0200

    Working pull

--- Combinatorial_optimization/algorithms/greedy_algorithm.py
+++ Combinatorial_optimization/algorithms/greedy_algorithm.py
@@ -1,16 +1,20 @@
 from graph.graph_utils import is_neighbour, sort_nodes_by_degree
 
- 
+
 def greedy_coloring(graph):
     num_nodes = len(graph)
     color = [0] * (num_nodes)
-    queue = sort_nodes_by_degree(graph)
+    queue = [i for i in range(1, num_nodes)]
+    # queue = sort_nodes_by_degree(graph)
 
     for node in queue:
-        lowest = 1
+        used = set()
         for i in range(1, num_nodes):
-            if is_neighbour(graph, node, i) and color[i] == lowest:
-                lowest += 1
+            if is_neighbour(graph, node, i) and color[i] != 0:
+                used.add(color[i])
+        lowest = 1
+        while lowest in used:
+            lowest += 1
         color[node] = lowest
 
-    return color[1:]
+    return color[1:], max(color[1:])
