56595566b53b38667434a6ae0e0a6a7cb703b646
SleepyFirebird <jakubowski.lolo@gmail.com>
2026-04-21 10:56:19 +0200

    reduction of folder structure

--- ATM_Project/include/PathResolver.h
+++
@@ -1,8 +0,0 @@
-#ifndef PATHRESOLVER_H
-#define PATHRESOLVER_H
-
-#include <string>
-
-std::string resolvePath(const std::string& relativePath);
-
-#endif // PATHRESOLVER_H
--- ENUM/README.txt
+++
@@ -1,3 +0,0 @@
-    source /Users/sleepyfirebird/Studia/Repozytoria/ENUM2/venv/bin/activate
-    deactivate
-    [0.599999, 0.600001] 1e-3
